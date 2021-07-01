181e1016910105293e40a1aacdd46a1eb9183512
kinz-up <berksmile@gmail.com>
2021-07-02 08:59:02 +0900

    chore: modify default background color for game board

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 import Wood from '../../assets/wood.png';
 import GridContainer from '../../containers/board/GridContainer';
 import { palette } from '../../lib/styles/palette';
-import Grid from './Grid';
-import Stone from './Stone';
 
 function Board() {
   return (
@@ -15,6 +13,7 @@ function Board() {
 }
 
 const BoardBlock = styled.div`
+  background-color: ${palette.gray[9]};
   background-image: url(${Wood});
   background-size: cover;
   filter: hue-rotate(-30deg) grayscale(0.1) sepia(0.8) brightness(80%);
